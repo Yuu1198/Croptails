26fa5b61f8f122ee40d65160230ac025e149b76f
Nadia <nadia.hofer1998@outlook.com>
2025-07-17 12:47:56 +0200

    Inventory Panel
    Inventory Panel UI and add collectables to it.

--- scenes/components/collectable_component.gd
+++ scenes/components/collectable_component.gd
@@ -5,5 +5,6 @@ extends Area2D
 
 func _on_body_entered(body):
 	if body is Player:
+		InventoryManager.add_collectable(collectable_name) #add collectable to inventory
 		print("Collected: ", collectable_name)
 		get_parent().queue_free()
---
+++ scripts/globals/inventory_manager.gd
@@ -0,0 +1,16 @@
+extends Node
+
+var inventory: Dictionary = Dictionary()
+
+signal inventory_changed
+
+func add_collectable(collectable_name: String) -> void:
+	inventory.get_or_add(collectable_name) #add collectable name to inventory dictonary
+	
+	#if nothing in inventory that matches name
+	if inventory[collectable_name] == null:
+		inventory[collectable_name] = 1 #then instantiate item and add 1
+	else:
+		inventory[collectable_name] += 1 #inctement value
+		
+	inventory_changed.emit()
